b8241a5191e4bcb6bde4f982bba12236d7d7c187
partoneplay <partoneplay@icloud.com>
2017-10-14 14:20:35 +0800

    mywiki

---
+++ conf/users.auth.php
@@ -0,0 +1,12 @@
+# users.auth.php
+# <?php exit()?>
+# Don't modify the lines above
+#
+# Userfile
+#
+# Format:
+#
+# login:passwordhash:Real Name:email:groups,comma,seperated
+
+
+dyb:$1$kHIJTu1h$/xJLPJzgy2p1LD4E31CCC1:dyb:partoneplay@163.com:admin,user
